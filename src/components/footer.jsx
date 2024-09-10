4afe7e9e0476de5f7ca417450b781d035e7bce86
Your Name <you@example.com>
2024-09-10 15:27:14 +0530

    untrack client folder

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function footer() {
+  return (
+<div class="card text-center">
+  <div class="card-footer text-body-secondary">
+    footer
+  </div>
+</div>
+  )
+}
+
+export default footer
